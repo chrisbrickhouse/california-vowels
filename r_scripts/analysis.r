74d79abb59f13ad9aceb45c7c0cd2909c1c766fd
Christian Brickhouse <ctb77@stanford.edu>
2019-02-25 15:13:42 -0800

    Add PraatSauce data and plots with comments.

--- r_scripts/analysis.r
+++ r_scripts/analysis.r
@@ -29,19 +29,30 @@ paths <- c(
   )
 )
 
+data.sauce = load_sauce('./data/sac_spectral_measures.txt')
+data.normed = make_norm(sauce)
 
-data = load_data(paths)
-data$site = as.factor(data$site)
-data$segment = as.factor(data$segment)
+data.formant = load_data(paths)
+data.formant$site = as.factor(data.formant$site)
+data.formant$segment = as.factor(data.formant$segment)
 
-f1_model = ssmodel(f1_bark ~ index*segment*site*sex,data=data)
+# Vowel space plot
+ggplot(data.normed,aes(x=nF2,y=nF1,color=Vowel,label=Vowel)) +
+  geom_point() +
+  scale_y_reverse(position = "right") + 
+  scale_x_reverse(position = "top") +
+  geom_density_2d()
 
-site_labels <- c(SAC="Sacramento",SAL="Salinas")
-a = plot_spline(f1_model,site_labels,ylab="Predicted F1 (Bark)")
-print(a)
-
-ggplot(data,aes(x=index,group=token,color=segment)) +
+# Formant Track plot
+ggplot(data.formant,aes(x=index,group=token,color=segment)) +
   geom_line(aes(y=f1_bark)) +
   geom_line(aes(y=f2_bark)) +
   geom_line(aes(y=f3_bark)) +
   facet_wrap(~site)
+
+# Smoothing Spline Plot
+f1_model = ssmodel(f1_bark ~ index*segment*site*sex,data=data.formant)
+
+site_labels <- c(SAC="Sacramento",SAL="Salinas")
+a = plot_spline(f1_model,site_labels,ylab="Predicted F1 (Bark)")
+print(a)
