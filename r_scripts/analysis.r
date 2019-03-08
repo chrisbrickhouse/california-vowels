d04afb70d005c51966654cfc7cef2113e0de9f0c
Christian Brickhouse <ctb77@stanford.edu>
2019-03-08 11:05:42 -0800

    Removing some unneeded lines of code.

--- r_scripts/analysis.r
+++ r_scripts/analysis.r
@@ -17,6 +17,7 @@ data.formant$segment = as.factor(data.formant$segment)
 
 data.v_angles = vowel_angles(data.normed)
 data.cosangles = compute_vspace(data.v_angles)
+plot_vangles(data.v_angles)
 
 # See number of vowels per site
 table(data.cleannormed%>%select(site,Vowel))
@@ -30,28 +31,3 @@ m = ssmodel(F1~index*segment*site,ssdata)
 m2 = ssmodel(F2~index*segment*site,ssdata)
 m3 = ssmodel(F3~index*segment*site,ssdata)
 
-# Vowel space plot
-ggplot(data.normed,aes(x=nF2,y=nF1,color=Vowel,label=Vowel)) +
-  #geom_text() + # uncomment to have vowels labelled with words
-  geom_point() +
-  scale_y_reverse(position = "right") + 
-  scale_x_reverse(position = "top") +
-  geom_density_2d()
-
-ggplot(data.formant,aes(x=index,group=token,color=segment)) +
-  geom_line(aes(y=f1_bark)) +
-  geom_line(aes(y=f2_bark)) +
-  geom_line(aes(y=f3_bark)) +
-  facet_wrap(~site)
-
-plot_vangles(data.v_angles)
-
-# Smoothing Spline Plot
-f1_model = ssmodel(f1_bark ~ index*segment*site*sex,data=data.formant)
-
-site_labels <- c(SAC="Sacramento",SAL="Salinas")
-a = plot_spline(f1_model,site_labels,ylab="Predicted F1 (Bark)")
-print(a)
-
-# Proof that concatenation works
-all.equal(data.sauce,load_sauce())
