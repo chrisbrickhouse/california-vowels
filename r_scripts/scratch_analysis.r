be0a43e17cd8ff3e63086006d5e0abb240a48deb
Christian Brickhouse <ctb77@stanford.edu>
2019-04-26 17:50:43 -0700

    WIP: change in vowel dynamics over time

--- r_scripts/scratch_analysis.r
+++ r_scripts/scratch_analysis.r
@@ -34,4 +34,24 @@ ggplot(dct_coeffs%>%drop_na(gender),aes(y=DCT3,x=site,fill=gender))+
   geom_boxplot()+
   facet_wrap(~token)
 
+F1_dct_coeffs = get_dct(data.clean.sauce.demo,"F1")%>%
+  filter(!site %in% c("MER","RED"))%>%
+  drop_na(DCT1,DCT2,DCT3,DCT4,birthyear,site)%>%
+  unite(id,site,last,first,remove = FALSE)
+F2_dct_coeffs = get_dct(data.clean.sauce.demo,"F2")%>%
+  filter(!site %in% c("MER","RED"))%>%
+  drop_na(DCT1,DCT2,DCT3,DCT4,birthyear,site)%>%
+  unite(id,site,last,first,remove = FALSE)
+f1_dct_preds = dct_predictions(data.clean.sauce.demo,"F1",4)%>%mutate(age_bin = ntile(birthyear,3))%>%drop_na(age_bin)%>%group_by(age_bin)%>%mutate(cF1=F1-mean(F1,na.rm=T),cpF1=pF1-mean(F1,na.rm=T))
+f2_dct_preds = dct_predictions(data.clean.sauce.demo,"F2",3)%>%mutate(age_bin = ntile(birthyear,3))%>%drop_na(age_bin)%>%group_by(age_bin)%>%mutate(cF2=F2-mean(F2,na.rm=T),cpF2=pF2-mean(F2,na.rm=T))
 
+ggplot(f1_dct_preds%>%filter(!site %in% c("RED","MER"))) +
+  geom_smooth(aes(x=index,y=cF1,color=token,linetype="Observed"),method = 'loess')+
+  geom_smooth(aes(x=index,y=cpF1,color=token,linetype="Predicted"),method = 'loess',se=F)+
+  labs(x="Index", y="F1 (Hz)") +
+  facet_wrap(~age_bin)
+ggplot(f2_dct_preds%>%filter(!site %in% c("RED","MER"))) +
+  geom_smooth(aes(x=index,y=cF2,color=token,linetype="Observed"),method = 'loess')+
+  geom_smooth(aes(x=index,y=cpF2,color=token,linetype="Predicted"),method = 'loess',se=F)+
+  labs(x="Index", y="F2 (Hz)") +
+  facet_wrap(~age_bin)
